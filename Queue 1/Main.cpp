72f77c72b68ac8aa2572885cb4c04381b6d7c017
Araik1 <iaraik99@gmail.com>
2019-05-05 00:26:05 +0900

    Final version

---
+++ Queue 1/Main.cpp
@@ -0,0 +1,55 @@
+﻿#include "Queue.h"
+#include <iostream>
+
+using namespace  std;
+/*Демонстраци работы класса queue*/
+int main() {
+	queue<int> q;
+
+	cout << q.size() << endl;
+
+	q.push(1);
+	q.push(2);
+	q.push(3);
+
+	cout << q.size() << endl;
+
+
+	cout << q.pop() << " ";
+	cout << q.pop() << " ";
+	cout << q.pop() << " ";
+
+	cout << endl;
+
+	cout << q.size() << endl;
+
+	q.push(4);
+	q.push(5);
+	q.push(6);
+
+	cout << q.size() << endl;
+
+	cout << "first element " << q.front() << endl;
+	cout << "last element " << q.back() << endl;
+
+
+	cout << q.size() << endl;
+	
+	queue<int> w(q);
+
+	w.pop();
+
+	cout << w.size() << endl;
+
+	cout << q.size() << endl;
+
+	queue<queue<int>> d;
+	d.push(q); 
+
+	const queue<int> f(8);
+
+	cout << f.size();
+
+	system("pause");
+	return 0;
+}
